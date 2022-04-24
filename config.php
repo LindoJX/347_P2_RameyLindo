0d2cdf573aec145dd9c5d238f0f68bf0e2755045
LindoJX <91221525+LindoJX@users.noreply.github.com>
2022-04-24 16:30:40 -0400

    changed auth_type in config.php to HTTP

--- config.php
+++ config.php
@@ -5,9 +5,10 @@ define('DB_SERVER', 'localhost');
 define('DB_USERNAME', 'RameyTL');
 define('DB_PASSWORD', 'password123!');
 define('DB_NAME', 'users');
- 
+
 /* Attempt to connect to MySQL database */
 try{
+    $cfg['Servers'][$i]['auth_type'] = 'HTTP';
     $pdo = new PDO("mysql:host=" . DB_SERVER . ";dbname=" . DB_NAME, DB_USERNAME, DB_PASSWORD);
     // Set the PDO error mode to exception
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
