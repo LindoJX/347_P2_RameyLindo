fa4a50252468a46b83ffceece584b6e3e15d8832
LindoJX <91221525+LindoJX@users.noreply.github.com>
2022-05-01 10:28:08 -0400

    Replaced some code in accountinfo.php for a test

--- accountinfo.php
+++ accountinfo.php
@@ -1,60 +1,111 @@
+// <?php
+// // Include config file
+// require_once "config.php";
+//
+// // Define variables and initialize with empty values
+// $interest = $length = "";
+// $interest_err = $length_err = "";
+//
+// // Processing form data when form is submitted
+// if($_SERVER["REQUEST_METHOD"] == "POST"){
+//
+//     // Validate username
+//     if(empty(trim($_POST["interest"]))){
+//         $interest_err = "Please enter an interest.";
+//     } else{
+//         // Prepare a select statement
+//         $sql = "SELECT id FROM users WHERE username = :username";
+//
+//         if($stmt = $pdo->prepare($sql)){
+//             // Bind variables to the prepared statement as parameters
+//             $stmt->bindParam(":username", $param_username, PDO::PARAM_STR);
+//
+//             // Set parameters
+//             $param_username = trim($_POST["username"]);
+//
+//             // Attempt to execute the prepared statement
+//             if($stmt->execute()){
+//                     $username = trim($_POST["username"]);
+//             }
+//
+//             // Close statement
+//             unset($stmt);
+//         }
+//     }
+//
+//
+//     // Check input errors before inserting in database
+//     if(empty($interest_err) && empty($time_err)){
+//
+//         // Prepare an insert statement
+//         $sql = "INSERT INTO interests (username, interest, length) VALUES (:username, :interest, :length)";
+//
+//         if($stmt = $pdo->prepare($sql)){
+//             // Bind variables to the prepared statement as parameters
+//             $stmt->bindParam(":username", $param_username, PDO::PARAM_STR);
+//             $stmt->bindParam(":interest", $param_interest, PDO::PARAM_STR);
+//             $stmt->bindParam(":length", $param_length, PDO::PARAM_STR);
+//
+//             // Set parameters
+//             $param_username = $username;
+//             $param_interest = $interest;
+//             $param_length = $length;
+//
+//             // Attempt to execute the prepared statement
+//             if($stmt->execute()){
+//                 // Redirect to login page
+//                 header("location: accountinfo.php");
+//             } else{
+//                 echo "Oops! Something went wrong. Please try again later.";
+//             }
+//
+//             // Close statement
+//             unset($stmt);
+//         }
+//     }
+//
+//     // Close connection
+//     unset($pdo);
+// }
+//
+// ?>
+
+
+
 <?php
 // Include config file
 require_once "config.php";
 
 // Define variables and initialize with empty values
-$interest = $length = "";
-$interest_err = $length_err = "";
+$username = $password = $confirm_password = "";
+$username_err = $password_err = $confirm_password_err = "";
 
 // Processing form data when form is submitted
 if($_SERVER["REQUEST_METHOD"] == "POST"){
- 
+
     // Validate username
-    if(empty(trim($_POST["interest"]))){
-        $interest_err = "Please enter an interest.";
+    if(empty(trim($_POST["username"]))){
+        $username_err = "Please enter a username.";
+    } elseif(!preg_match('/^[a-zA-Z0-9_]+$/', trim($_POST["username"]))){
+        $username_err = "Username can only contain letters, numbers, and underscores.";
     } else{
         // Prepare a select statement
         $sql = "SELECT id FROM users WHERE username = :username";
-        
+
         if($stmt = $pdo->prepare($sql)){
             // Bind variables to the prepared statement as parameters
             $stmt->bindParam(":username", $param_username, PDO::PARAM_STR);
-            
+
             // Set parameters
             $param_username = trim($_POST["username"]);
-            
+
             // Attempt to execute the prepared statement
             if($stmt->execute()){
+                if($stmt->rowCount() == 1){
+                    $username_err = "This username is already taken.";
+                } else{
                     $username = trim($_POST["username"]);
-            }
-
-            // Close statement
-            unset($stmt);
-        }
-    }
-    
-    
-    // Check input errors before inserting in database
-    if(empty($interest_err) && empty($time_err)){
-        
-        // Prepare an insert statement
-        $sql = "INSERT INTO interests (username, interest, length) VALUES (:username, :interest, :length)";
-         
-        if($stmt = $pdo->prepare($sql)){
-            // Bind variables to the prepared statement as parameters
-            $stmt->bindParam(":username", $param_username, PDO::PARAM_STR);
-            $stmt->bindParam(":interest", $param_interest, PDO::PARAM_STR);
-            $stmt->bindParam(":length", $param_length, PDO::PARAM_STR);
-
-            // Set parameters
-            $param_username = $username;
-            $param_interest = $interest;
-            $param_length = $length;
-            
-            // Attempt to execute the prepared statement
-            if($stmt->execute()){
-                // Redirect to login page
-                header("location: accountinfo.php");
+                }
             } else{
                 echo "Oops! Something went wrong. Please try again later.";
             }
@@ -63,16 +114,62 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
             unset($stmt);
         }
     }
-    
+
+    $exp = "(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}";
+
+    // Validate password
+    if(empty(trim($_POST["password"]))){
+        $password_err = "Please enter a password.";
+    } else if (!preg_match($exp, $_POST["password"])) {
+        $password_err = "Must contain at least one number and one uppercase and lowercase letter";
+    } elseif(strlen(trim($_POST["password"])) < 6){
+        $password_err = "Password must have atleast 6 characters.";
+    } else{
+        $password = trim($_POST["password"]);
+    }
+
+    // Validate confirm password
+    if(empty(trim($_POST["confirm_password"]))){
+        $confirm_password_err = "Please confirm password.";
+    } else{
+        $confirm_password = trim($_POST["confirm_password"]);
+        if(empty($password_err) && ($password != $confirm_password)){
+            $confirm_password_err = "Password did not match.";
+        }
+    }
+
+    // Check input errors before inserting in database
+    if(empty($username_err) && empty($password_err) && empty($confirm_password_err)){
+
+        // Prepare an insert statement
+        $sql = "INSERT INTO users (username, password) VALUES (:username, :password)";
+
+        if($stmt = $pdo->prepare($sql)){
+            // Bind variables to the prepared statement as parameters
+            $stmt->bindParam(":username", $param_username, PDO::PARAM_STR);
+            $stmt->bindParam(":password", $param_password, PDO::PARAM_STR);
+
+            // Set parameters
+            $param_username = $username;
+            $param_password = password_hash($password, PASSWORD_DEFAULT); // Creates a password hash
+
+            // Attempt to execute the prepared statement
+            if($stmt->execute()){
+                // Redirect to login page
+                header("location: login.php");
+            } else{
+                echo "Oops! Something went wrong. Please try again later.";
+            }
+
+            // Close statement
+            unset($stmt);
+        }
+    }
+
     // Close connection
     unset($pdo);
 }
-
 ?>
-
-
-
-
 
 
 
